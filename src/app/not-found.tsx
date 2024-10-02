8d778d589ad257e094f0c4c2f0463c932ebaafc0
hugo414 <hugohaban@s.zochova.sk>
2024-10-02 13:54:20 +0200

    initial

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+// src/app/podmienky/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function NotFound() {
+
+  return(
+    <Box>
+        <Typography> There's nothing like </Typography>
+    </Box>
+  );
+}
---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,11 @@
+// src/app/o-mne/page.tsx
+
+import Typography from "@mui/material/Typography";
+
+export default function Home() {
+
+  return(
+    <Typography> Stránka o mne </Typography>
+
+  );
+}
